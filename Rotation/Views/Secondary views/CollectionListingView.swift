a897390a87c1c7983ed39b1f3604e9bec73f112d
Mack Slevin <mack.slevin@gmail.com>
2023-12-04 15:17:39 -0800

    Style collection view list rows

--- Rotation/Views/Secondary views/CollectionListingView.swift
+++ Rotation/Views/Secondary views/CollectionListingView.swift
@@ -27,15 +27,55 @@ struct CollectionListingView: View {
                         NavigationLink {
                             MusicEntityDetailView(musicEntity: musicEntity)
                         } label: {
-                            VStack(alignment: .leading) {
-                                Text(musicEntity.title)
-                                    .fontWeight(.semibold)
-                                Text(musicEntity.artistName)
-                                Text(musicEntity.played ? "Played" : "Unplayed")
-                                    .font(.caption).foregroundStyle(.secondary)
+                            HStack(spacing: 12) {
+                                VStack {
+                                    Spacer()
+                                    Circle()
+                                        .frame(width: 12)
+                                        .foregroundStyle(Color.accentColor.gradient)
+                                        .opacity(musicEntity.played ? 0 : 1)
+                                    Spacer()
+                                }
+                                
+                                musicEntity.image.resizable().scaledToFit()
+                                    .padding(musicEntity.imageData == nil ? 10 : 0)
+                                    .frame(width: 80, height: 80)
+                                    .background(Color.secondary)
+                                    .clipShape(RoundedRectangle(cornerRadius: Utility.defaultCorderRadius(small: true)))
+                                
+                                VStack(alignment: .leading) {
+                                    Text(musicEntity.title)
+                                        .font(Font.displayFont(ofSize: 18))
+                                        .lineLimit(3)
+                                    Text(musicEntity.artistName)
+                                        .foregroundStyle(.secondary)
+                                }
+                            }
+                            
+                        }
+                        .listRowSeparator(.hidden)
+                        .listRowBackground(Color.clear)
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                withAnimation {
+                                    musicEntity.played.toggle()
+                                }
+                            } label: {
+                                if musicEntity.played {
+                                    Label("Unplayed", systemImage: "play.slash")
+                                } else {
+                                    Label("Played", systemImage: "play")
+                                }
+                            }
+                            .tint(Color.accentColor)
+                        }
+                        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                            Button(role: .destructive) {
+                                modelContext.delete(musicEntity)
+                            } label: {
+                                Label("Delete", systemImage: "trash")
                             }
                         }
-                        .listRowBackground(Color.clear)
                     }
                 }
             }
