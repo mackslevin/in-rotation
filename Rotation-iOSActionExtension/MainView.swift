0db5c207ef544026c6f533115678d30be8c878e7
Mack Slevin <mack.slevin@gmail.com>
2023-12-26 15:22:37 -0800

    meh

---
+++ Rotation-iOSActionExtension/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Rotation-iOSActionExtension
+//
+//  Created by Mack Slevin on 12/26/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
