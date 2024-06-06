0ed59f2c643bde429f4e19d0d7f5ad168498a661
Mack Slevin <mack.slevin@gmail.com>
2024-06-06 11:24:51 -0700

    Set up basics for new primary view with tab view inside and a view model

---
+++ Rotation/Types/View Models/PrimaryViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryViewModel.swift
+//  Rotation
+//
+//  Created by Mack Slevin on 6/6/24.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+class PrimaryViewModel {
+    var selectedTab = 1
+    
+    init() {
+        
+        
+        self.selectedTab = selectedTab
+    }
+}
